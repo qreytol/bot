0f35d95b09d346abc77ac33525b2937b63091928
Egorik068 <68874501+Egorik068@users.noreply.github.com>
2022-07-23 22:31:57 +0300

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -3,11 +3,11 @@ from DATETIME import date_time
 import datetime
 
 today = datetime.date.today()
-six_day = today + datetime.timedelta(hours=3, days=5)
-two_day = today + datetime.timedelta(hours=3, days=1)
-three_day = today + datetime.timedelta(hours=3, days=2)
-thour_day = today + datetime.timedelta(hours=3, days=3)
-five_day = today + datetime.timedelta(hours=3, days=4)
+six_day = today + datetime.timedelta(days=5)
+two_day = today + datetime.timedelta(days=1)
+three_day = today + datetime.timedelta(days=2)
+thour_day = today + datetime.timedelta(days=3)
+five_day = today + datetime.timedelta(days=4)
 dtimee = date_time()
 
 week_two = two_day.strftime('%A')
@@ -16,12 +16,19 @@ week_thour = thour_day.strftime('%A')
 week_five = five_day.strftime('%A')
 week_six = six_day.strftime('%A')
 mainMenu = InlineKeyboardMarkup(row_width=3)
+
+StartMenu = InlineKeyboardMarkup()
+
 oneBTN = InlineKeyboardButton(text=f'{dtimee.transweek(week_two)}', callback_data='right_weather')
 threeBTN = InlineKeyboardButton(text=f'{dtimee.transweek(week_three)}', callback_data='left_weather')
 thourBTN = InlineKeyboardButton(text=f'{dtimee.transweek(week_thour)}', callback_data='thourbtn')
 fiveBTN = InlineKeyboardButton(text=f'{dtimee.transweek(week_five)}', callback_data='fivebtn')
 sixBTN = InlineKeyboardButton(text=f'{dtimee.transweek(week_six)}', callback_data='sixbtn')
 twoBTN = InlineKeyboardButton(text='Сьогодні', callback_data='twobtn')
+
+startBTN = InlineKeyboardButton(text='Добавити', url='https://telegram.me/Arnold_new_chat_bot?startgroup=new')
+
+StartMenu.insert(startBTN)
 
 mainMenu.insert(oneBTN)
 mainMenu.insert(threeBTN)
